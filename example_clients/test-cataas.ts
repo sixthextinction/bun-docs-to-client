81afa9d5b1563b0ace193959aa9d5805e65599f5
Prithwish Nath <prithwish.nath@gmail.com>
2026-02-17 01:38:49 +0530

    Delete example clients (I'm not sure of the rights😅)

--- example_clients/test-cataas.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bun
-
-import { ApiClient } from '../generated/cataas_com/client.js';
-
-const client = new ApiClient('https://cataas.com');
-
-const cat = await client.cat_random(undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, true);
-console.log('🐱 Random cat:', cat);
